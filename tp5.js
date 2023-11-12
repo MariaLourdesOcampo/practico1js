595b57841db67539500463d5dacb0955313df5a7
MariaLourdesOcampo <143985878+MariaLourdesOcampo@users.noreply.github.com>
2023-11-12 11:36:05 -0300

    Update tp5.js

--- tp5.js
+++ tp5.js
@@ -1,3 +1,3 @@
-numero1 = parseInt (prompt('ingresa un numero'))
-numero2 = parseInt (prompt('ingresa otra numero'))
+let numero1 = parseInt (prompt('ingresa un numero'))
+let numero2 = parseInt (prompt('ingresa otra numero'))
 document.write(numero1 + numero2)
